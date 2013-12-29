6f2bc7d2c3ae7ae836732b8c3bc37fd4d93e759b
Rose Williams <rosew@binghamton.edu>
2013-12-28 20:53:55 -0500

    Comment and new method

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,2 +1,9 @@
+
+
 class Species < ActiveRecord::Base
+  # Return a status string of a particular species
+  def full_species
+    "#{name} #{identification} has #{(assimilated? ? ' ' : 'not')}been assimilated."
+
+  end
 end
